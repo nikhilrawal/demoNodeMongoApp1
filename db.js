c89292361f9daf00e50ef43ef1035a73c3652be9
nikhil <dtydhtb420@gmail.com>
2025-01-10 14:34:20 +0530

    version-2

--- db.js
+++ db.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
 require('dotenv').config()
-const mongoUrl = process.env.DB_URL2
+// const mongoUrl = process.env.DB_URL2
+// to use above, add url in env of atlas mongodb cluster, or else simply run  below locally by starting mongo db compass 
+const mongoUrl = 'mongodb://localhost:27017/mydb'
 mongoose.connect(mongoUrl)
 db = mongoose.connection
 db.on(('connected'), () => {
